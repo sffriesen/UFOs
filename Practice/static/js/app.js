70fe02a3d2bf025aaf5840fb52ab872dcfa86891
Susan Friesen <sf.friesen@gmail.com>
2021-12-02 11:07:14 -0800

    added app functions to build tables

--- Practice/static/js/app.js
+++ Practice/static/js/app.js
@@ -13,7 +13,7 @@ function buildTable(data) {
     data.forEach((dataRow) => {
         // tell JavaScript to find the <tbody> tag in the HTML and add a table row
         let row = tbody.append("tr");
-        
+
         // add a new function to loop through each field in the dataRow argument
         Object.values(dataRow).forEach((val) => {
             // append each value of the object ot a cell in the table
@@ -24,3 +24,29 @@ function buildTable(data) {
     });
 
 }
+
+// create a function for filtering data on click
+function handleClick() {
+    // create variables to hold date data
+    let date = d3.select("#datetime").property("value");
+    // set a default filter (the whole original table data)
+    let filteredData = tableData;
+
+    // if there is a date present, filter the data
+    if (date) {
+        // Apply `filter` to the table data to only keep the
+        // rows where the `datetime` value matches the filter value
+        filteredData = filteredData.filter(row => row.datetime === date);
+    };
+
+    // build the table using the filtered data
+    // @NOTE: If no date was entered, then filteredData will
+    // just be the original tableData.
+    buildTable(filteredData);
+}
+
+// Attach an event to listen for the form button
+d3.selectAll("#filter-btn").on("click:, handleClick");
+
+// Build the table when the page loads
+buildTable(tableData);
